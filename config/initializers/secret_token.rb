85fed42aecf64a46982d9b15fe5c85186f17213e
Smokin Joe <joe.ekiert@gmail.com>
2014-02-16 00:21:28 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homerpicks::Application.config.secret_key_base = '63cf795852581b80e89dc14537451f2685e25cdedc96666a2e9abd69aafd382e220a859e3cf7b3d554fb31b69f082ad9071c0687fa1f873fa8e46305ae040adb'
