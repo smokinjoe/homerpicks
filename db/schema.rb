796679ad1aa010bda4b5e6204ca44e5d4f701dee
Smokin Joe <joe.ekiert@gmail.com>
2014-02-26 03:50:25 +0000

    adding schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216001823) do
+ActiveRecord::Schema.define(version: 20140218043645) do
+
+  create_table "groups", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "groups", ["name"], name: "index_groups_on_name", using: :btree
+
+  create_table "groups_users", force: true do |t|
+    t.integer "user_id",  null: false
+    t.integer "group_id", null: false
+  end
+
+  add_index "groups_users", ["group_id"], name: "index_groups_users_on_group_id", using: :btree
+  add_index "groups_users", ["user_id"], name: "index_groups_users_on_user_id", using: :btree
+
+  create_table "predictions", force: true do |t|
+    t.integer  "user_id"
+    t.string   "team"
+    t.integer  "wins"
+    t.integer  "losses"
+    t.string   "tiebreaker"
+    t.boolean  "confirmed",  default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "predictions", ["user_id"], name: "index_predictions_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -24,10 +53,17 @@ ActiveRecord::Schema.define(version: 20140216001823) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "display_name",           default: "", null: false
+    t.integer  "role",                   default: 0,  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
