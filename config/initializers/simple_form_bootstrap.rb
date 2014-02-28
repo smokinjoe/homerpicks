1942c9a405b5d48d424b563c94d05a0e8e40db90
Smokin Joe <joe.ekiert@gmail.com>
2014-02-28 05:52:50 +0000

    modifications to help update the simple_form_for input wrappers (needs work)

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,16 +1,42 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, tag: 'div', class: 'form-group', error_class: 'error' do |b|
+  config.wrappers :bootstrap, tag: 'div', class: '', error_class: 'has-error', defaults: { input_html: { class: 'default_class' } } do |b|
     b.use :html5
+    b.use :min_max
+    b.use :maxlength
     b.use :placeholder
-    b.use :label
-    #b.wrapper tag: 'div', class: 'controls' do |ba|
-      b.use :input
-      b.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
-      b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
-    #end
+    #b.use :label
+    b.optional :pattern
+    b.optional :readonly
+    
+    b.wrapper tag: 'div', class: 'controls' do |input|
+      input.use :label # maybe move back out
+      input.use :input #, input_html: { class: 'form-control' }
+      input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+      input.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+    end
   end
 
+  # from https://github.com/plataformatec/simple_form/issues/857
+  config.wrappers :horizontal, tag: 'div', class: 'form-group', error_class: 'has-error', defaults: { input_html: { class: 'default_class' } }  do |b|
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
+    b.use :placeholder
+    b.optional :pattern
+    b.optional :readonly
+
+    b.wrapper tag: 'div', class: 'controls' do |input|
+      input.use :label, class: 'horizontal'
+      input.wrapper tag: 'div' do |prepend|
+         prepend.use :input, class: 'horizontal'
+      end
+      input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+      input.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+    end
+  end  
+  
+  # potentially deprecated
   config.wrappers :prepend, tag: 'div', class: "form-group", error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
@@ -24,6 +50,7 @@ SimpleForm.setup do |config|
     end
   end
 
+  # potentially deprecated
   config.wrappers :append, tag: 'div', class: "form-group", error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
