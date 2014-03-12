9a74b75a2ccecc14af542faf3385cd809a3b6572
Smokin Joe <joe.ekiert@gmail.com>
2014-03-12 01:50:58 +0000

    in progress dealie, most likely delete

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,3 +1,10 @@
 class Prediction < ActiveRecord::Base
   belongs_to :user
+  
+  #validate :check_confirmed
+  #def check_confirmed
+  #  if params
+  #errors.add(:base, 'error message')
+  #end
+  
 end
