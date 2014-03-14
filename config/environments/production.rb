e78a42599c66aed177160944fb6a0707d9a41f46
Smokin Joe <joe.ekiert@gmail.com>
2014-03-14 17:43:14 +0000

    setting ActionMailer informations for production environment to work with Heroku addon, SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,25 @@ Homerpicks::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'homerpicks.herokuapp.com' }
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default_url_options = { :host => 'homerpicks.herokuapp.com' }
+  
+  # another attempt from: http://stackoverflow.com/questions/4114835/heroku-devise-missing-host-to-link-to-please-provide-host-parameter-or-set-d/4118506#4118506
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
 
+  ActionMailer::Base.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'homerpicks.herokuapp.com' }
+  
+  
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
