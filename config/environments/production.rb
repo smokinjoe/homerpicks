530a11dd79d622efa69740961297406c6be075f4
Smokin Joe <joe.ekiert@gmail.com>
2014-03-14 01:31:26 +0000

    dummyish commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,8 @@ Homerpicks::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'homerpicks.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -77,8 +78,5 @@ Homerpicks::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'homerpicks.herokuapp.com' }
 
 end
